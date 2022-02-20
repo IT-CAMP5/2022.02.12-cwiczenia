2ecc74b1fcd1fefe5c02f7bce1a1b50e426b291f
Mateusz <mat.bereda@gmail.com>
2022-02-21 00:41:08 +0100

    init

---
+++ src/main/java/pl/camp/it/cwiczenia/Main.java
@@ -0,0 +1,11 @@
+package pl.camp.it.cwiczenia;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] tab = {1,2,3,4,5,6,7};
+
+        for(int i = tab.length-1; i >= 0; i--) {
+            System.out.println(tab[i]);
+        }
+    }
+}
